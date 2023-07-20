5ed45052cf545e5f7cfcd276bca441a2cfcffc40
abyscaria <52022944+abyscaria@users.noreply.github.com>
2023-07-20 10:45:17 +1000

    ver23 updates
    Ver3 updated

--- routes/index.js
+++ routes/index.js
@@ -18,9 +18,23 @@ const glossary = [
   {'ID': 5, 'Term': 'Automated Delivery Pipeline', 
   'Description': 'The automated Delivery Pipeline is a critical component of DevOps that involves the automated and continuous delivery of software updates to production environments. It is a software delivery practice that enables teams to streamline the release process, reduce errors and downtime, and deliver updates more quickly and reliably.',
   'References':'Sharma, A. (2021, November 16). What is Devops | A Comprehensive Guide to DevOps. Scaler Topics. https://www.scaler.com/topics/what-is-devops/'},
-]
+  {'ID': 6, 'Term': 'Lead time for Code Change', 
+  'Description': 'One of the critical DevOps metrics to track is lead time for changes. Not to be confused with cycle time (discussed below), lead time for changes is the length of time between when a code change is committed to the trunk branch and when it is in a deployable state. For example, when code passes all necessary pre-release tests. automated Delivery Pipeline is a critical component of DevOps that involves the automated and continuous delivery of software updates to production environments. It is a software delivery practice that enables teams to streamline the release process, reduce errors and downtime, and deliver updates more quickly and reliably.',
+  'References':'Atlassian. (n.d.). 4 Key DevOps Metrics to Know. Atlassian. Retrieved July 19, 2023, from https://www.atlassian.com/devops/frameworks/devops-metrics#:~:text=Lead%20time%20for%20changes&text=Not%20to%20be%20confused%20with'},
+  {'ID': 7, 'Term': 'Change Failure Rate', 
+  'Description': 'The change failure rate is the percentage of code changes that require hot fixes or other remediation after production. This does not measure failures caught by testing and fixed before code is deployed.',
+  'References':'Atlassian. (n.d.). 4 Key DevOps Metrics to Know. Atlassian. Retrieved July 19, 2023, from https://www.atlassian.com/devops/frameworks/devops-metrics#:~:text=Lead%20time%20for%20changes&text=Not%20to%20be%20confused%20with'},
+  {'ID': 8, 'Term': 'Deployment Frequency', 
+  'Description': 'Understanding the frequency of how often new code is deployed into production is critical to understanding DevOps success. Many practitioners use the term “delivery” to mean code changes that are released into a pre-production staging environment, and reserve “deployment” to refer to code changes that are released into production.',
+  'References':'Atlassian. (n.d.). 4 Key DevOps Metrics to Know. Atlassian. Retrieved July 19, 2023, from https://www.atlassian.com/devops/frameworks/devops-metrics#:~:text=Lead%20time%20for%20changes&text=Not%20to%20be%20confused%20with'},
+  {'ID': 9, 'Term': 'Mean Time to Recovery (MTTR)', 
+  'Description': 'Mean time to recovery (MTTR) measures how long it takes to recover from a partial service interruption or total failure. This is an important metric to track, regardless of whether the interruption is the result of a recent deployment or an isolated system failure. ',
+  'References':'Atlassian. (n.d.). 4 Key DevOps Metrics to Know. Atlassian. Retrieved July 19, 2023, from https://www.atlassian.com/devops/frameworks/devops-metrics#:~:text=Lead%20time%20for%20changes&text=Not%20to%20be%20confused%20with'},
+  {'ID': 10, 'Term': 'Cycle Time Reports', 
+  'Description': 'Cycle time reports allow project leads to establish a baseline for the development pipeline that can be used to evaluate future processes. When teams optimize for cycle time, developers typically have less work in progress and fewer inefficient workflows.',
+  'References':'Atlassian. (n.d.). 4 Key DevOps Metrics to Know. Atlassian. Retrieved July 19, 2023, from https://www.atlassian.com/devops/frameworks/devops-metrics#:~:text=Lead%20time%20for%20changes&text=Not%20to%20be%20confused%20with'},
 
-
+];
 
 
 /* GET home page. */
